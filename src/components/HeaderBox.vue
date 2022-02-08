7d5e5c5b36d0825b69ab1c72d4497452ef9d8a60
Cezar <cezarlantosgmail.com>
2022-02-08 19:03:57 +0100

    header

---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="main-container">
+    <div class="background">
+
+        <nav-bar />
+        
+        <div class="info">
+            <h1>Our Team<span>.</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam excepturi quasi consequuntur magnam cupiditate necessitatibus earum!</p>
+
+            <div class="button">
+                <div class="button-info">
+                    <span class="border-black">READ MORE</span>
+                    <i class="fas fa-long-arrow-alt-right"></i> 
+                </div>
+
+                <div class="button-buy">
+                    <span class="border-white">PURCHASE</span>
+                    <i class="fas fa-long-arrow-alt-right"></i> 
+                </div>
+            </div>
+        
+        </div>
+    
+        <i class="fas fa-long-arrow-alt-left absolute left"></i>
+        <i class="fas fa-long-arrow-alt-right absolute right"></i>
+
+    </div>
+</div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue';
+export default {
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+@import '../style/HeaderBox.scss';
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div>
+            <img src="@/assets/logo-img-01.png" alt="">
+        </div>
+        <div>
+            <ul>
+                <li><i class="fas fa-long-arrow-alt-right"></i>HOMES</li>
+                <li><i class="fas fa-long-arrow-alt-right"></i>PAGES</li>
+                <li><i class="fas fa-long-arrow-alt-right"></i>BLOG</li>
+                <li><i class="fas fa-long-arrow-alt-right"></i>SHOP</li>
+                <li><i class="fas fa-long-arrow-alt-right"></i>EVENTS</li>
+                <li><i class="fas fa-long-arrow-alt-right"></i>ELEMENTS</li>
+                <li><i class="fas fa-search"></i></li>
+                <li><i class="fas fa-list"></i></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+@import '../style/NavBar.scss';
+</style>
