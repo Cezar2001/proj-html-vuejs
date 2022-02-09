9295b3ed4d48d1555acf3499c9127dd2f309bf3a
Cezar <cezarlantosgmail.com>
2022-02-09 12:09:02 +0100

    jumbo

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -7,7 +7,67 @@
         </div>
 
         <div class="skills">
+            <div>
+                <img id="test" src="../assets/svg-0.svg" alt="">
+                <h3>Knowing</h3>
+                <span>Lorem ipsum dolor sit amet, quis cosect.</span>
+            </div>
 
+            <div>
+                <img id="test" src="../assets/svg-1.svg" alt="">
+                <h3>Selling</h3>
+                <span>Lorem ipsum dolor sit amet, quis cosect.</span>
+            </div>
+
+            <div>
+                <img id="test" src="../assets/svg-2.svg" alt="">
+                <h3>Learning</h3>
+                <span>Lorem ipsum dolor sit amet, quis cosect.</span>
+            </div>
+
+            <div>
+                <img id="test" src="../assets/svg-3.svg" alt="">
+                <h3>Contact</h3>
+                <span>Lorem ipsum dolor sit amet, quis cosect.</span>
+            </div>
+        </div>
+
+        <div class="up">
+            <i class="fas fa-long-arrow-alt-up"></i>
+        </div>
+
+        <div class="founder-img">
+            <div class="button">
+                <i class="fas fa-long-arrow-alt-left"></i> 
+            </div>
+
+            <div class="button">
+                <i class="fas fa-long-arrow-alt-right"></i> 
+            </div>
+        </div>
+
+        <div class="founder-info">
+            <h1>Jason Bickford</h1>
+            
+            <span>Founder and Executive Director</span>
+            
+            <div class="line"></div>
+            
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, nobis, exercitationem...</span>
+            
+            <div>
+                <i class="fab fa-linkedin-in"></i>
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-twitter"></i>
+            </div>
+        </div>
+
+        <div class="circle">
+            <!-- <img id="test" src="../assets/svg-4.svg" alt=""> -->
+        </div>
+
+        <div class="up two-up">
+            <i class="fas fa-long-arrow-alt-up"></i>
         </div>
     </div>
 </template>
@@ -20,42 +80,5 @@ export default {
 
 <style lang="scss" scoped>
 @import '../style/variables.scss';
-.container{
-    background-color: $grey-color;
-    height: 700px;
-    display: flex;
-    justify-content: center;
-
-    .specialities{
-        width: 1100px;
-        height: 500px;
-        top: 570px;
-        text-align: center;
-        position: absolute;
-        background-color: $white-color;
-
-        h1{
-            margin: 50px 0 10px 0;
-
-            span{
-                color: $orange-color;
-                border: none;
-            }
-        }
-
-        span{
-            color: $light-grey-color;
-        }
-
-        .line{
-            border-bottom: 2px $orange-color solid;
-            width: 50px;
-            margin: 15px 0 0 530px;
-        }
-    }
-
-    .skills{
-        
-    }
-}
+@import '../style/JumboTron.scss';
 </style>
